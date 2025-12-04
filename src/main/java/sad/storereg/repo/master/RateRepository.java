01d11aec3c40db8551a7c54cffa1b7b35a2a64d5
Hazel <hazelmanners777@gmail.com>
2025-12-04 16:28:36 +0530

    Added units rates fetch endpoint

--- src/main/java/sad/storereg/repo/master/RateRepository.java
+++ src/main/java/sad/storereg/repo/master/RateRepository.java
@@ -1,10 +1,13 @@
 package sad.storereg.repo.master;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sad.storereg.models.master.Rate;
 
@@ -22,6 +25,13 @@ public interface RateRepository extends JpaRepository<Rate, Long>{
 	        Long itemId, Long subItemId,
 	        Integer yearRangeId
 	);
+	@Query("SELECT r FROM Rate r WHERE r.item.id = :itemId " +
+		       "AND ((:subItemId IS NULL AND r.subItem IS NULL) OR r.subItem.id = :subItemId) " +
+		       "AND r.yearRange.id = :yearRangeId")
+		List<Rate> findRatesByItemAndOptionalSubItem(
+		        @Param("itemId") Long itemId,
+		        @Param("subItemId") Long subItemId,
+		        @Param("yearRangeId") Integer yearRangeId);
 
 
 
--- src/main/java/sad/storereg/services/master/MasterDataServices.java
+++ src/main/java/sad/storereg/services/master/MasterDataServices.java
@@ -1,14 +1,22 @@
 package sad.storereg.services.master;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import sad.storereg.dto.master.UnitRateDTO;
+import sad.storereg.dto.master.UnitRequestDTO;
+import sad.storereg.exception.UnauthorizedException;
 import sad.storereg.models.master.Category;
+import sad.storereg.models.master.Rate;
 import sad.storereg.models.master.Unit;
+import sad.storereg.models.master.YearRange;
 import sad.storereg.repo.master.CategoryRepository;
+import sad.storereg.repo.master.RateRepository;
 import sad.storereg.repo.master.UnitRepository;
+import sad.storereg.repo.master.YearRangeRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +25,10 @@ public class MasterDataServices {
 	private final CategoryRepository categoryRepo;
 	
 	private final UnitRepository unitRepo;
+	
+	private final RateRepository rateRepository;
+	
+	private final YearRangeRepository yearRangeRepository;
 	
 	public List<Category> getCategories() {
 		try {
@@ -29,6 +41,27 @@ public class MasterDataServices {
 	public List<Unit> getUnits() {
 		try {
 				return unitRepo.findAll();
+			}catch(Exception ex) {
+			throw ex;
+		}
+    }
+	
+	public List<UnitRateDTO> getUnitsRates(UnitRequestDTO request) {
+		try {
+			int year = request.getPurchaseDate().getYear();
+			YearRange yearRange = yearRangeRepository.findByStartYearLessThanEqualAndEndYearGreaterThanEqual(year, year).orElseThrow(()->new UnauthorizedException("Rate for year "+year+" has not been defined in master data"));
+
+			List<Rate> rates = rateRepository.findRatesByItemAndOptionalSubItem(request.getItemId(), request.getSubItemId(), yearRange.getId());
+			// Map Rate entities to UnitRateDTO
+	        return rates.stream()
+	                .map(rate -> {
+	                    UnitRateDTO dto = new UnitRateDTO();
+	                    dto.setUnitId(rate.getUnit().getId());
+	                    dto.setUnitName(rate.getUnit().getUnit());
+	                    dto.setRate(rate.getRate());
+	                    return dto;
+	                })
+	                .toList();
 			}catch(Exception ex) {
 			throw ex;
 		}
